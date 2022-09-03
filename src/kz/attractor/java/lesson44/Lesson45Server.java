5b961d3ea6caec57d96a186fbf18f21d3c0e626f
Andrei Likhoded <liberty102and8131186@gmail.com>
2022-09-03 21:30:43 +0600

    new work

---
+++ src/kz/attractor/java/lesson44/Lesson45Server.java
@@ -0,0 +1,10 @@
+package kz.attractor.java.lesson44;
+
+import java.io.IOException;
+
+public class Lesson45Server extends Lesson44Server{
+
+    public Lesson45Server(String host, int port) throws IOException {
+        super(host, port);
+    }
+}
